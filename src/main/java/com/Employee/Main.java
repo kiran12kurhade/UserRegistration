ef5c318a60d0f2b4b0491b1a9c75747ab8527d14
kiran12kuhrade <Github@1999>
2024-10-10 16:06:56 +0530

    Last Name start with cap and has mimimum 3 charater

--- src/main/java/com/Employee/Main.java
+++ src/main/java/com/Employee/Main.java
@@ -7,6 +7,11 @@ public class Main {
         // Regular expression to check if the first name starts with a capital letter and has at least 3 characters
         return firstName.matches("^[A-Z][a-zA-Z]{2,}$");
     }
+    public static boolean isValidLastName(String lastName) {
+        // Regular expression to check if the last name starts with a capital letter and has at least 3 characters
+        return lastName.matches("^[A-Z][a-zA-Z]{2,}$");
+    }
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -18,6 +23,15 @@ public class Main {
             System.out.println("First name is valid.");
         } else {
             System.out.println("First name is invalid. It should start with a capital letter and have at least 3 characters.");
+        }
+
+        System.out.println("Enter a valid last name:");
+        String lastName = scanner.nextLine();
+
+        if (isValidLastName(lastName)) {
+            System.out.println("Last name is valid.");
+        } else {
+            System.out.println("Last name is invalid. It should start with a capital letter and have at least 3 characters.");
         }
 
         scanner.close();
